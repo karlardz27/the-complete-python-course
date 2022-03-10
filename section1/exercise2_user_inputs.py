1b53fa47c6ff8a0819bc7ae02eb607099091941d
Karla Rodríguez <75685216+karlardz27@users.noreply.github.com>
2022-03-10 12:36:32 -0600

    Adding exercise2_user_inputs.py

---
+++ section1/exercise2_user_inputs.py
@@ -0,0 +1,10 @@
+# First, ask the user for their name. Then, print out the greeting "Hello, NAME"
+# Remember the uppercase H, the comma, and the space between words!
+your_name = input("Enter your name: ")
+print("Hello, " + your_name)
+
+# Secondly, ask the user for their age and convert it into a number.
+# Then, print out how many months that equals to (you'll have to multiply the age by 12).
+your_age = int(input("Enter your age: "))
+months = your_age * 12
+print (months)
